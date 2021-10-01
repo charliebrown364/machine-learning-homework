204f0aa11dcbb00111fe085d59ae162cafa27327
Charlie Weinberger <70112746+charliebrown364@users.noreply.github.com>
2021-10-01 09:11:54 +0000

    addded to decision_tree

--- 001/gradient_descent_parameter_fitting/gradient_descent_parameter_fitting.py
+++ 001/gradient_descent_parameter_fitting/gradient_descent_parameter_fitting.py
@@ -3,8 +3,11 @@ import time
 alpha = 0.001
 delta = 0.01
 num_iterations = 10000
-points = [[0, 0], [1, 1], [2, 4]]
-starting_point = [0, 2]
+# points = [[0, 0], [1, 1], [2, 4]]
+# starting_point = [0, 2]
+
+points = [[0, 1], [2, 5]]
+starting_point = [2, 1]
 
 def rss(b_0, b_1):
     rss = 0
@@ -17,14 +20,17 @@ def gradient_descent_parameter_fitting(n):
     for i in range(num_iterations):
         b_0 -= alpha * (rss(b_0 + delta, b_1) - rss(b_0 - delta, b_1)) / (2 * delta)
         b_1 -= alpha * (rss(b_0, b_1 + delta) - rss(b_0, b_1 - delta)) / (2 * delta)
-        if n == 0 and i < 2:
+        # if n == 0 and i < 2:
+        if i == 0:
            print("py iteration: (b_0, b_1) =", (b_0, b_1))
 
-start_time = time.time()
+gradient_descent_parameter_fitting(0)
 
-for n in range(10):
-    gradient_descent_parameter_fitting(n)
+# start_time = time.time()
 
-end_time = time.time()
+# for n in range(10):
+#     gradient_descent_parameter_fitting(n)
 
-print("Python:", (end_time - start_time) / 10)
+# end_time = time.time()
+
+# print("Python:", (end_time - start_time) / 10)
--- 002/decision_tree.py
+++ 002/decision_tree.py
@@ -5,8 +5,7 @@ def calc_entropy(points):
     num_class_0 = sum([point['class'] == 0 for point in points])
     num_class_1 = sum([point['class'] == 1 for point in points])
     
-    if num_class_0 == 0 or num_class_1 == 0:
-        return 0
+    if num_class_0 == 0 or num_class_1 == 0: return 0
     
     p0 = num_class_0 / len(points)
     p1 = num_class_1 / len(points)
@@ -59,13 +58,15 @@ class Node:
     def find_best_split(self):
         weighted_avgs = [split.get_weighted_avg() for split in self.splits]
         return self.splits[weighted_avgs.index(min(weighted_avgs))]
-
-    def is_pure(self):
-        return calc_entropy(self.points) == 0
+    
+    def is_pure_enough(self, min_size_to_split):
+        all_the_same = len(set([(point['x'], point['y']) for point in self.points])) == 1
+        return calc_entropy(self.points) == 0 or len(self.points) < min_size_to_split or all_the_same
 
 class DecisionTree:
-    def __init__(self):
+    def __init__(self, min_size_to_split=1):
         self.root = None
+        self.min_size_to_split = min_size_to_split
         
     def fit(self, points):
 
@@ -80,15 +81,18 @@ class DecisionTree:
 
             for i in [0, 1]:
                 temp_node = current_node.find_best_split().children[i]
-                if not temp_node.is_pure():
+                if not temp_node.is_pure_enough(self.min_size_to_split):
                     nodes_to_do.insert(i, temp_node)
     
     def predict(self, point):
         
         current_node = self.root
-        while not current_node.is_pure():
+        while not current_node.is_pure_enough(self.min_size_to_split):
             best_split = current_node.find_best_split()
             child_index = point[best_split.dim] > best_split.midpoint # False = 0, True = 1
             current_node = best_split.children[child_index]
         
-        return current_node.points[0]['class']
+        classes = [point['class'] for point in current_node.points]
+        # if len([i == 0 for i in classes]) == len([i == 1 or i in classes]):
+        #     return random.randint(0, 1)
+        return max(set(classes), key = classes.count)
